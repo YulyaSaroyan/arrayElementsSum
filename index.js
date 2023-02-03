dea71780940ded5bc23c763ec6730716bd48c678
Yulya <yulya.saroyan@bk.ru>
2023-02-03 04:35:43 -0800

    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+    let cartItemsPricesSum = cartItemsPrices.reduce((acc, cur) => (parseFloat(acc) + parseFloat(cur)).toFixed(2));
+
+    if (cartItemsPricesSum == WINNING_SUM) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = doesCartWinPrize;
